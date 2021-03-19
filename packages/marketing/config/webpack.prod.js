03bda03cc7ff8a6941ea43c8e9c143bbebad48b8
Kiattisak Chaisomboon <bass.kiattisak@gmail.com>
2021-03-19 20:18:13 +0700

    update

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,4 +1,4 @@
-const  {merge} = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
