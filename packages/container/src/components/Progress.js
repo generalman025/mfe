3eda3f2b9b185c784ef637e17b2acaace0366f55
Kiattisak Chaisomboon <bass.kiattisak@gmail.com>
2021-03-20 13:17:08 +0700

    Added auth subapp

---
+++ packages/container/src/components/Progress.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {makeStyles, createStyles} from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const useSytles = makeStyles((theme) => {
+    return createStyles({
+        bar: {
+            width: '100%',
+            '& > * + *': {
+                marginTop: theme.spacing(2)
+            }
+        }
+    })
+});
+
+export default () => {
+    const classes = useSytles();
+    return (
+        <div className={classes.bar}>
+            <LinearProgress />
+        </div>
+    );
+}
